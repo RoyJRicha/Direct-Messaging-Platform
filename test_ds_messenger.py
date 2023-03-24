aa6c5a044868e9aef7107ea19ac621e8558334a9
Roy Richa <rricha@uci.edu>
2023-03-23 22:50:19 -0700

    Finished covering the DM class, now I will work on covering the Direct Messenger class.

--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -2,3 +2,10 @@ import unittest
 from ds_messenger import DirectMessage, DirectMessenger
 
 class TestDirectMessage(unittest.TestCase):
+    def test_direct_message_init(self):
+        dm = DirectMessage()
+        self.assertIsNone(dm.recipient)
+        self.assertIsNone(dm.message)
+        self.assertIsNone(dm.timestamp)
+
+    
