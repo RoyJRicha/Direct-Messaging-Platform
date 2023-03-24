3335bef7f2947f621c8a6c2978cb7aeb8968c34d
Roy Richa <rricha@uci.edu>
2023-03-23 22:55:15 -0700

    Finished the setup for the DMsnger initializations for that class.

--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -8,4 +8,14 @@ class TestDirectMessage(unittest.TestCase):
         self.assertIsNone(dm.message)
         self.assertIsNone(dm.timestamp)
 
-    
+class TestDirectMessenger(unittest.TestCase):
+
+    def setUp(self):
+        self.dm = DirectMessenger(dsuserver="168.235.86.101", username="RandomUser1234", password="testpassword")
+
+    def test_direct_messenger_init(self):
+        self.assertIsNone(self.dm.token)
+        self.assertEqual(self.dm.username, "RandomUser1234")
+        self.assertEqual(self.dm.password, "testpassword")
+        self.assertEqual(self.dm.dsuserver, "168.235.86.101")
+        self.assertEqual(self.dm.port, 3021)
