4311fbc5d1d50dd26362e51c71e343450331189e
Roy Richa <rricha@uci.edu>
2023-03-24 11:10:02 -0700

    Added more test cases that required importing mock so I can mimic a Connection error or any other that I may not be able to test directly. This was for ds_messenger, now I am at 77% test coverage. I am getting closer.

--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest.mock import MagicMock
 from ds_messenger import DirectMessage, DirectMessenger
+import socket
 
 class TestDirectMessage(unittest.TestCase):
     def test_direct_message_init(self):
@@ -24,6 +26,10 @@ class TestDirectMessenger(unittest.TestCase):
         result = self.dm.send("Hello, World!", "TestRecipient")
         self.assertTrue(result)
 
+    def test_invalid_send(self):
+        result = self.dm.send(1234, True)
+        self.assertFalse(result)
+
     def test_retrieve_new(self):
         result = self.dm.retrieve_new()
         self.assertIsInstance(result, list)
@@ -31,6 +37,40 @@ class TestDirectMessenger(unittest.TestCase):
     def test_retrieve_all(self):
         result = self.dm.retrieve_all()
         self.assertIsInstance(result, list)
+
+    def test_invalid_ip_address(self):
+        dm = DirectMessenger(dsuserver="invalid_ip", username="RandomUser1234", password="testpassword")
+        result, _ = dm.retrieve_token()
+        self.assertFalse(result)
+
+    def test_invalid_input_types(self):
+        dm = DirectMessenger(dsuserver=123, username=345, password=False)
+        result, _ = dm.retrieve_token()
+        self.assertFalse(result)
+
+    def test_timeout_error(self):
+        with unittest.mock.patch('socket.socket.connect', side_effect=socket.timeout()):
+            dm = DirectMessenger(dsuserver="168.235.86.101", username="RandomUser1234", password="testpassword")
+            result, _ = dm.retrieve_token()
+            self.assertFalse(result)
+
+    def test_connection_refused_error(self):
+        with unittest.mock.patch('socket.socket.connect', side_effect=ConnectionRefusedError()):
+            dm = DirectMessenger(dsuserver="168.235.86.101", username="RandomUser1234", password="testpassword")
+            result, _ = dm.retrieve_token()
+            self.assertFalse(result)
+
+    def test_socket_gaierror(self):
+        with unittest.mock.patch('socket.socket.connect', side_effect=socket.gaierror()):
+            dm = DirectMessenger(dsuserver="168.235.86.101", username="RandomUser1234", password="testpassword")
+            result, _ = dm.retrieve_token()
+            self.assertFalse(result)
+
+    def test_os_error(self):
+        with unittest.mock.patch('socket.socket.connect', side_effect=OSError()):
+            dm = DirectMessenger(dsuserver="168.235.86.101", username="RandomUser1234", password="testpassword")
+            result, _ = dm.retrieve_token()
+            self.assertFalse(result)
 
 '''
 if __name__ == '__main__':
